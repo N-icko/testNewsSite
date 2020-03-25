2fc3fcc76e9565b877b3853c6c357cf6a6a63f41
Nickolay Kuzmyn <55614030+N-icko@users.noreply.github.com>
2020-03-25 10:24:28 +0200

    Add files via upload

---
+++ testTask/assets/js/index.js
@@ -0,0 +1,7 @@
+//Изменение цвета активной ссылки:
+$('.header-top-nav__list-item-link').click(function () {
+	$(this).toggleClass("active-header-link");
+});
+
+$('.weather__city').click(function () {
+});
