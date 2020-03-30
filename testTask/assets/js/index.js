9b0d96e3df77275a3ab5accb670b49551d12b6da
Nickolay <kuzmin.study@gmail.com>
2020-03-30 16:45:17 +0300

    refactor

--- testTask/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-//Изменение цвета активной ссылки:
-$('.header-top-nav__list-item-link').click(function () {
-	$(this).toggleClass("active-header-link");
-});
-
-$('.weather__city').click(function () {
-});
